6fd75fc55a830681b293d708a6ca17a453c64a7d
luis2910 <lui.perezc@duocuc.cl>
2023-04-27 15:32:57 -0400

    ajustes carrito

--- carrito.js
+++
@@ -1,8 +0,0 @@
-// Obtener los elementos del DOM
-const cartItemsEl = document.querySelector('.cart-items');
-const cartTotalEl = document.querySelector('#cart-total');
-const emptyCartBtn = document.querySelector('#empty-cart-btn');
-const addToCartBtns = document.querySelectorAll('.add-to-cart-btn');
-
-// Inicializar el carrito como un objeto vacío
-let cart = {};
